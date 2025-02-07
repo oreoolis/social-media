0e276d9d2112880ed27917879530ee9ae7f54746
oreoolis <dwaytero02@gmail.com>
2025-02-07 17:43:13 +0800

    updated domain name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Title from './components/title';
 import Links from './components/links';
 import OtherSocials from './components/otherSocials';
 import Footer from './components/footer';
-
-
 
 function App() {
   return (
